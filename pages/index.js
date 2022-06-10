21c75a84bcf94589b449b0ef5a5b1e88613fc681
michaelmunter <iammunter@gmail.com>
2022-06-10 13:23:25 +0200

    test branch message

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Munter-Munter-Munter</p>
+        <p>Muntarus DeMuntio</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
